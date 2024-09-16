702489b74f10dfcd945cc3df5446965fdc2130f8
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-15 20:30:18 -0700

    Add authentication and loading states to UI components
    Integrate `ConvexClientProvider` to handle authentication state in layout. Add `Spinner` component for loading states and update Navbar and Heading components to reflect authenticated, unauthenticated, and loading states.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import React from "react";
+import {ConvexClientProvider} from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,15 +30,17 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                    storageKey="luminotes-theme"
-                >
-                    {children}
-                </ThemeProvider>
+                <ConvexClientProvider>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                        storageKey="luminotes-theme"
+                    >
+                        {children}
+                    </ThemeProvider>
+                </ConvexClientProvider>
             </body>
         </html>
     );
