ed4a7b6d45114d5f5add5a6c453a514dc9a62c11
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-17 07:47:58 -0700

    Add document archiving, trash, and restore functionality
    Implemented archive, restore, and delete features for documents. Added UI components for trash management, including a dropdown in Item and a trash modal in Navigation. Extended Convex backend with mutations and queries for handling document states.

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {ChevronsLeft, MenuIcon, PlusCircle, Search, Settings} from "lucide-react";
+import {ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash} from "lucide-react";
 import React, {ElementRef, useEffect, useRef, useState} from "react";
 import {useMediaQuery} from "usehooks-ts";
 import {usePathname} from "next/navigation";
@@ -11,6 +11,12 @@ import {api} from "@/convex/_generated/api";
 import {Item} from "@/app/(main)/_components/Item";
 import {toast} from "sonner";
 import {DocumentList} from "@/app/(main)/_components/Document-List";
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent
+} from "@/components/ui/popover"
+import {TrashBox} from "@/app/(main)/_components/Trash-Box";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -143,6 +149,22 @@ export const Navigation = () => {
                 </div>
                 <div className="mt-4">
                     <DocumentList />
+                    <Item
+                        onClick={handleCreate}
+                        icon={Plus}
+                        label="Add a page"
+                    />
+                    <Popover>
+                        <PopoverTrigger className="w-full mt-4">
+                            <Item label="Trash" icon={Trash} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                            className="p-0 w-72"
+                            side={isMobile ? "bottom" : "right"}
+                        >
+                            <TrashBox />
+                        </PopoverContent>
+                    </Popover>
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
