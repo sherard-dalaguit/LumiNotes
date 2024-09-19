8fa9754f48eccb275d35e43af15f7be4f89ead76
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-19 11:53:31 -0700

    minor code refactors

--- components/editor.tsx
+++ components/editor.tsx
@@ -2,7 +2,7 @@
 
 import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import { useCreateBlockNote } from "@blocknote/react";
-import "@blocknote/core/style.css";
+import "@blocknote/shadcn/style.css";
 import { BlockNoteView } from "@blocknote/shadcn";
 import { useTheme } from "next-themes";
 import { useEdgeStore } from "@/lib/edgestore";
