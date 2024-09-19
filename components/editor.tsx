b3994e69f7cdcb098c8431b8cc2d206baad6d93a
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-18 18:43:32 -0700

    Remove custom editor dark mode styles
    Deleted the redundant `editor_style.css` file and removed its import in `editor.tsx`. These custom styles are now unnecessary due to the implementation of improved theme handling.

--- components/editor.tsx
+++ components/editor.tsx
@@ -6,7 +6,6 @@ import "@blocknote/core/style.css";
 import { BlockNoteView } from "@blocknote/shadcn";
 import { useTheme } from "next-themes";
 import { useEdgeStore } from "@/lib/edgestore";
-import "./editor_style.css"
 
 interface EditorProps {
     onChange?: (value: string) => void;
