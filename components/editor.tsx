66ecd8b7ed77d0885a0a95789db1c8875a866285
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-18 17:40:10 -0700

    Add rich text editor with content saving functionality
    Introduced a new rich text editor component to the document page with dynamic import and content-saving capabilities. Added necessary dependencies and implemented content uploading to the database. Improved code styling and refactored some parts for better readability and functionality.

---
+++ components/editor.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
+import "@blocknote/shadcn/style.css";
+import { BlockNoteView } from "@blocknote/shadcn";
+import { useTheme } from "next-themes";
+interface EditorProps {
+  onChange?: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+import { useEdgeStore } from "@/lib/edgestore";
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({ file })
+    return response.url;
+  }
+
+  const editor: BlockNoteEditor = useCreateBlockNote({
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
+    uploadFile: handleUpload
+  });
+
+  const uploadToDatabase = () => {
+    if (onChange) {
+        onChange(JSON.stringify(editor.document, null, 2));
+    }
+  }                 ;
+
+  return (
+    <BlockNoteView
+      onChange={uploadToDatabase}
+      editor={editor}
+      editable={editable}
+      theme={resolvedTheme === "dark" ? "dark" : "light"}
+    />
+  );
+};
+
+export default Editor;
